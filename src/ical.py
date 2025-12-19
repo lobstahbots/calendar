32bc3aaa116263fb5ba65154930a97614d547bf5
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2025-12-19 11:47:33 -0500

    try an update

--- src/ical.py
+++ src/ical.py
@@ -7,11 +7,13 @@ def get_icalendar(events: list[Event]) -> icalendar.Calendar:
     cal = icalendar.Calendar()
     cal.color = "#c40000"
     cal.calendar_name = "Lobstah Bots Calendar"
+    cal.add("X-WR-CALNAME", cal.calendar_name)
     for event in events:
         ical_event = icalendar.Event()
         ical_event.uid = event.id
         ical_event.add("summary", event.title)
         ical_event.start = event.start_date
+        ical_event.color = "#c40000"
         if event.end_date:
             ical_event.end = event.end_date + timedelta(days=1)
         if event.location:
